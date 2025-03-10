82d91a6a57891b209f50a6dcdd515241e7f01f29
Hershraj Niranjani <60162641+talkierbox@users.noreply.github.com>
2025-03-09 17:23:58 -0700

    Update robogrammar_search_spec.py

--- robot_search/robogrammar_search_spec.py
+++ robot_search/robogrammar_search_spec.py
@@ -5,27 +5,24 @@ Make only small changes. Try to make the code short.
 
 The robot is formed through a list of numbers, and are automatically designed symmetrically.
 """
-
-import numpy as np
 import funsearch
 import re
 from typing import List
 import requests
-
 
 METHOD_MATCHER = re.compile(r"def robot_v\d\(.*?\) -> List\[int\]:(?:\s*(?:[ \t]*(?!def|#|`|').*(?:\n|$)))+")
 METHOD_NAME_MATCHER = re.compile(r"robot_v\d+")
 method_str = "def robot_v"
 
 @funsearch.run
-def evaluate_robot(design) -> float:
+def evaluate_robot(task="RidgedTerrainTask") -> float:
   """Returns the best reward managed by the robot design. Done via calling the robogrammar API
   """
   url = "http://127.0.0.1:5555/simulate"
   payload = {
-      "task": "RidgedTerrainTask",
+      "task": task,
       "grammar_file": "data/designs/grammar_apr30.dot",
-      "rule_sequence": design,
+      "rule_sequence": robot(),
       "jobs": 8,
       "optim": True,
       "episodes": 1, # using multiple episodes causes FCValueEstimator to crash apparently --- keep at 1
